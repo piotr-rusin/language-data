9b69b902127e4277dd68111b41ea5e5a60b1668c
Piotr Rusin <piotr.rusin88@gmail.com>
2020-06-04 22:56:55 +0200

    Implement data model for language

---
+++ src/main/java/com/github/piotr_rusin/language_data/language/LanguageRow.java
@@ -0,0 +1,80 @@
+package com.github.piotr_rusin.language_data.language;
+
+import com.github.piotr_rusin.language_data.DataRow;
+import com.opencsv.bean.CsvBindByName;
+
+public class LanguageRow extends DataRow {
+
+    @CsvBindByName(column="Name")
+    private String name;
+    @CsvBindByName(column="Macroarea")
+    private String macroarea;
+    @CsvBindByName(column="Latitude")
+    private String latitude;
+    @CsvBindByName(column="Longitude")
+    private String longitude;
+    @CsvBindByName(column="Glottocode")
+    private String glottocode;
+    @CsvBindByName(column="ISO639P3code")
+    private String iso639P3code;
+    @CsvBindByName(column="Family")
+    private String family;
+    @CsvBindByName(column="Subfamily")
+    private String subfamily;
+    @CsvBindByName(column="Genus")
+    private String genus;
+    @CsvBindByName(column="ISO_codes")
+    private String isoCodes;
+    @CsvBindByName(column="Samples_100")
+    private String samples100;
+    @CsvBindByName(column="Samples_200")
+    private String samples200;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMacroarea() {
+        return macroarea;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getGlottocode() {
+        return glottocode;
+    }
+
+    public String getIso639P3code() {
+        return iso639P3code;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getSubfamily() {
+        return subfamily;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public String getIsoCodes() {
+        return isoCodes;
+    }
+
+    public String getSamples100() {
+        return samples100;
+    }
+
+    public String getSamples200() {
+        return samples200;
+    }
+}
